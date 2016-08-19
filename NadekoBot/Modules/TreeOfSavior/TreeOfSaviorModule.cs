84ba8aebe6af016aed03d0ab47c2fd62ba48e4fd
Kyoryo Ishigami <franz.rio19@gmail.com>
2016-08-19 19:00:58 +0700

    create command undone

--- NadekoBot/Modules/TreeOfSavior/TreeOfSaviorModule.cs
+++ NadekoBot/Modules/TreeOfSavior/TreeOfSaviorModule.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Discord.Modules;
+using NadekoBot.Modules.Permissions.Classes;
+using Discord.Commands;
 
 namespace NadekoBot.Modules.TreeOfSavior
 {
@@ -12,6 +14,17 @@ namespace NadekoBot.Modules.TreeOfSavior
         public override string Prefix { get; } = NadekoBot.Config.CommandPrefixes.TreeOfSavior;
         public override void Install(ModuleManager manager)
         {
+            manager.CreateCommands("", x =>
+            {
+                x.AddCheck(PermissionChecker.Instance);
+                x.CreateCommand(Prefix + "tosbase items")
+                    .Description("Display list of items from tosbase")
+                    .Parameter("query", ParameterType.Unparsed)
+                    .Do(async e =>
+                    {
+                        var tag 
+                    })
+            });
             throw new NotImplementedException();
         }
     }
