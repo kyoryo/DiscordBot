54f2da4d8d7dbef421b603fab3eeb631cf1789e2
Kyoryo Ishigami <franz.rio19@gmail.com>
2016-08-19 17:55:46 +0700

    add tree of savior module

---
+++ NadekoBot/Modules/TreeOfSavior/TreeOfSaviorModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Modules;
+
+namespace NadekoBot.Modules.TreeOfSavior
+{
+    internal class TreeOfSaviorModule : DiscordModule
+    {
+        public override string Prefix { get; } = NadekoBot.Config.CommandPrefixes.TreeOfSavior;
+        public override void Install(ModuleManager manager)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
